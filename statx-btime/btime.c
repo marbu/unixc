39e5ea9220d12fe1eec0cacb652fdfa34f27de76
Martin Bukatovic <martin.bukatovic@gmail.com>
2018-12-22 18:00:02 +0100

    statx-btime: drop unnecessary includes

--- statx-btime/btime.c
+++ statx-btime/btime.c
@@ -2,11 +2,8 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <error.h>
 
-#include <sys/types.h>
 #include <sys/stat.h>
-#include <unistd.h>
 #include <fcntl.h>
 
 int main(int argc, char**argv)
